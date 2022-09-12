41914f17791b3921f8633f897c295ad40061a2e5
Tom Kemper <tom.kemper@hu.nl>
2022-09-12 10:59:21 +0200

    Ids uit json halen

--- canvasapi/main.py
+++ canvasapi/main.py
@@ -2,7 +2,19 @@ from credentials import token
 from CanvasAPI import CanvasAPI
 
 proxy = 'http://localhost:8888'
-api = CanvasAPI('https://canvas.hu.nl/api/v1/', token, proxy)
+api = CanvasAPI('https://canvas.hu.nl/api/v1/', token)
 
-for course in api.get_pages('courses'):
-    print(course)
+# for course in api.get_pages('courses'):
+#   print(course)
+
+
+import json
+
+ids = []
+with open('./../inno-data.json') as f:
+    inno_data = json.load(f)
+    print(inno_data)
+    for p in inno_data['projects']:
+        ids.append(p['id'])
+
+print(ids)
