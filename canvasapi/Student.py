ead552f05ced5618c9b00c5de4ae56dee4d8b74b
Tom <tomkempernl@gmail.com>
2022-09-13 17:25:05 +0200

    En nu doet de eerste indruk het ook

--- canvasapi/Student.py
+++ canvasapi/Student.py
@@ -1,15 +1,19 @@
 class Student:
-    def __init__(self, sid, name):
+    def __init__(self, sid, name, sections=None):
         self.id = sid
         self.name = name
-        self.sections = []
+        self.sections = sections
+        if not sections:
+            self.sections = []
 
     def to_json(self):
         return self.__dict__
+
+    def __str__(self):
+        return f'Student({self.id}, {self.name}, {self.sections})'
 
     @staticmethod
     def from_dict(data_dict):
         new_student = Student(data_dict['id'], data_dict['name'])
         new_student.sections = new_student.sections + data_dict['sections']
         return new_student
-
--- canvasapi/main.py
+++ canvasapi/main.py
@@ -15,9 +15,8 @@ api = CanvasAPI('https://canvas.hu.nl/api/v1/', token)
 
 with open('./../inno-ids.json') as f:
     this_year_id = json.load(f)['main']
-    this_year_raw = api.get('courses/32665')
+    this_year_raw = api.get(f'courses/{this_year_id}')
     this_year = InnovationCourse(this_year_raw['id'], this_year_raw['name'])
-
 
 def get_students_from_course(inno_course, target_section):
     inno_sections_raw = api.get_pages(f'courses/{inno_course.id}/sections')
@@ -27,9 +26,9 @@ def get_students_from_course(inno_course, target_section):
     for inno_enrolment_raw in inno_enrolments_raw:
         if inno_enrolment_raw['course_section_id'] in sd_sections:
             student = Student(inno_enrolment_raw['user']['id'], inno_enrolment_raw['user']['name'])
+            student.sections.append(target_section)
             students.append(student)
     return students
 
-
 sd_students = get_students_from_course(this_year, 'SD')
-print(sd_students)
+print(*sd_students)
